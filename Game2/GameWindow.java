1b229494580de2849a6211ab7172e890bc30581f
Cameron MacMillan <csm31@st-andrews.ac.uk>
2026-03-28 16:48:31 +0000

    Less buggy

--- Game2/GameWindow.java
+++ Game2/GameWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.BorderLayout;
 
 public class GameWindow extends JFrame {
     public GameWindow(MemoryPlus memory) {
--- GameUI/Display.java
+++ GameUI/Display.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 import Game1.Square;
+import Game2.MemoryPlus;
 import Game3.Game3UI;
 
 import java.awt.*;
@@ -83,8 +84,7 @@ public class Display {
         button.setFocusPainted(false);
         button.setBorder(new EmptyBorder(10, 20, 10, 20));
         button.setCursor(new Cursor(Cursor.HAND_CURSOR));
-setScreen(panel);
-    }
+        setScreen(panel);
         button.addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent e) { button.setBackground(HOVER_COLOR); }
             public void mouseExited(MouseEvent e) { button.setBackground(TOOLBAR_BG); }
@@ -98,22 +98,25 @@ setScreen(panel);
         menu.setBackground(DARK_BG);
 
         menu.add(createStyledItem("Reaction Speed", this::startGameOne));
-        menu.add(createStyledItem("Game 2", () -> System.out.println("G2 clicked")));
+        menu.add(createStyledItem("Game 2", this::startGameTwo));
+        menu.add(createStyledItem("Game 3", this::startGameThree));
         menu.addSeparator();
         menu.add(createStyledItem("Quit Program", () -> System.exit(0)));
         return menu;
     }
 
     private void startGameOne() {
-        System.out.println("hi");
         Square square = new Square();
         square.changePlacement();
         setScreen(square.getGamePanel());
     }
 
+    private void startGameTwo() {
+        //MemoryPlus mem = new MemoryPlus();
+    }
+
     private void startGameThree() {
-        System.out.println("hi");
-        setScreen(new Game3UI());
+        //setScreen(new Game3UI());
     }
 
 
