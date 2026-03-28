19b48761dd80aaf47e05c55b2b3a803744fb5676
Daniel <dm390@st-andrews.ac.uk>
2026-03-28 14:08:41 +0000

    Added timer class

--- Testing/Main.java
+++ Testing/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Timer timer = new Timer();
+        while (true) {
+            System.out.println(timer.timeSinceStart());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
