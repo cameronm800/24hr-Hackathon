32f8fde02b68eb5a5154fa15d610de29141744ed
Cameron MacMillan <csm31@st-andrews.ac.uk>
2026-03-28 16:35:24 +0000

    blob

--- Game1/Square.java
+++ Game1/Square.java
@@ -1,6 +1,8 @@
 package Game1;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Random;
 public class Square {
     private boolean isRed = true;
