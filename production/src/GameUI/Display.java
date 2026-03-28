b64f21d344bc176a03d1f04bf71088ab2bb600ce
blop <blopblop913@gmail.com>
2026-03-28 11:03:22 +0000

    Made basic UI.

---
+++ production/src/GameUI/Display.java
@@ -0,0 +1,14 @@
+package GameUI;
+import javax.swing.*;
+public class Display {
+
+    public Display() {
+        JFrame frame = new JFrame("Display");
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void showWindow() {
+
+    }
+}
--- production/src/Main.java
+++ production/src/Main.java
@@ -1,6 +1,9 @@
+import GameUI.Display;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("WHAAA");
+        Display display = new Display();
+        display.showWindow();
     }
 }
