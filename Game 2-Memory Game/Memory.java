141e36f94ee0a9eec9c56d2577261676b9b4b2f6
Valentijn van der Sterren <vetvds1@st-andrews.ac.uk>
2026-03-28 11:49:55 +0000

    added colors to the Memory class

--- Game 2-Memory Game/Memory.java
+++ Game 2-Memory Game/Memory.java
@@ -3,9 +3,12 @@ import java.util.Random;
 
 public class Memory {
     private int[][] grid;
+    private int[][] colors;
      
     public Memory(int k, int r) {
-        this.grid = new int[k][k]; 
+        this.grid = new int[k][k];
+        this.colors = new int[k][3];
+        this.generateColors(k); 
         this.generateGrid(k, r);
     }
 
@@ -20,8 +23,26 @@ public class Memory {
         }
     }
 
+    public void generateColors(int k) {
+        Random rand = new Random();
+
+        for (int i = 0; i < k; i++) {
+            int r = rand.nextInt(255);
+            int g = rand.nextInt(255);
+            int b = rand.nextInt(255);
+
+            this.colors[i][0] = r;
+            this.colors[i][1] = g;
+            this.colors[i][2] = b;
+        }
+    }
+
     public void addNum(int i, int j, int n) {
         this.grid[i][j] = n;
+    }
+
+    public int[][] getColors() {
+        return this.colors;
     }
 
     public int[][] getGrid() {
