043ccae94a9e57433e4c0ec9b2520e0b2020ce85
blop <blopblop913@gmail.com>
2026-03-28 17:22:53 +0000

    WOrks?

--- Game1/Square.java
+++ Game1/Square.java
@@ -1,16 +1,18 @@
 package Game1;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-public class Square {
-    private boolean isRed = true;
-    private int score = 0;
-    private final int UISize = 580;
 
-    private int[] coords = new int[2];
-    private int squareSize = UISize / 2;
+//WARNING AI SLOP!! BEWAREW!!
+
+public class Square {
+    private int score = 0;
+    private final int UISize = 800;
 
     private JPanel gamePanel;
     private JLabel scoreLabel;
@@ -18,25 +20,31 @@ public class Square {
     private Timer randomTimer;
     private Random random = new Random();
 
-    public Square() {
-        gamePanel = new JPanel() {
+    private List<SquareEntity> squares = new ArrayList<>();
 
+    public Square() {
+        addNewSquare();
+
+        gamePanel = new JPanel() {
+            @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.setColor(Color.BLACK);
                 g.fillRect(0, 0, getWidth(), getHeight());
 
-                g.setColor(isRed ? Color.RED : Color.GREEN);
-                g.fillRect(coords[0], coords[1], squareSize, squareSize);
+                for (SquareEntity sq : squares) {
+                    g.setColor(sq.isRed ? Color.RED : Color.GREEN);
+                    g.fillRect(sq.coords[0], sq.coords[1], sq.squareSize, sq.squareSize);
+                }
             }
 
+            @Override
             public Dimension getPreferredSize() {
-                return new Dimension(800,600);
+                return new Dimension(800, 600);
             }
         };
         gamePanel.setLayout(null);
         gamePanel.setBackground(Color.BLACK);
-        System.out.println("HIasdf");
 
         scoreLabel = new JLabel("Score: 0");
         scoreLabel.setForeground(Color.WHITE);
@@ -45,86 +53,91 @@ public class Square {
         gamePanel.add(scoreLabel);
 
         randomTimer = new Timer(1000, e -> {
-            System.out.println("HI");
             toggleColorRandomly();
         });
-
         randomTimer.start();
 
         gamePanel.addMouseListener(new MouseAdapter() {
+            @Override
             public void mousePressed(MouseEvent e) {
                 int mousePosX = e.getX();
                 int mousePosY = e.getY();
 
-                if (mousePosX >= coords[0] && mousePosX <= coords[0] + squareSize && mousePosY >= coords[1] && mousePosY <= coords[1] + squareSize) {
-                    Pressed();
-                    isRed = !isRed;
-                    changePlacement();
+                for (int i = squares.size() - 1; i >= 0; i--) {
+                    SquareEntity sq = squares.get(i);
 
-                    scoreLabel.setText("Score" + getScore());
-                    scoreLabel.repaint();
-                    gamePanel.repaint();
+                    if (mousePosX >= sq.coords[0] && mousePosX <= sq.coords[0] + sq.squareSize
+                            && mousePosY >= sq.coords[1] && mousePosY <= sq.coords[1] + sq.squareSize) {
+
+                        handleSquareClick(sq);
+                        break;
+                    }
                 }
             }
         });
-        System.out.println(score);
     }
 
-
-    public boolean getColour() {
-        return this.isRed;
-    }
-
-    public void setColour(boolean isRed) {
-        this.isRed = isRed;
-    }
-
-    public void Pressed() {
-        if (isRed) {
-            score -=1;
-            squareSize += (int) UISize / 100;
+    private void handleSquareClick(SquareEntity sq) {
+        if (sq.isRed) {
+            score -= 1;
+            sq.squareSize += UISize / 50;
+        } else {
+            score += 1;
+            sq.squareSize -= UISize / 50;
         }
-        else {
-            score +=1;
-            squareSize -= (int) UISize / 100;
+
+        sq.isRed = !sq.isRed;
+        changePlacement(sq);
+
+        int targetSquareCount = 1 + Math.max(0, score / 5);
+        while (squares.size() < targetSquareCount) {
+            addNewSquare();
         }
+
+        scoreLabel.setText("Score: " + score);
+        gamePanel.repaint();
+    }
+
+    private void addNewSquare() {
+        SquareEntity newSq = new SquareEntity();
+        newSq.squareSize = UISize / 8;
+        newSq.isRed = true;
+        changePlacement(newSq);
+        squares.add(newSq);
+    }
+
+    private void changePlacement(SquareEntity sq) {
+        sq.coords[0] = random.nextInt(Math.max(1, UISize - sq.squareSize));
+        sq.coords[1] = random.nextInt(Math.max(1, UISize - sq.squareSize));
     }
 
     private void toggleColorRandomly() {
-        isRed = random.nextBoolean();
+        for (SquareEntity sq : squares) {
+            sq.isRed = random.nextBoolean();
+        }
+
         int scoreBonus = Math.min(3500, score * 100);
         int maxRandom = 4000 - scoreBonus;
         int nextDelay = 500 + random.nextInt(Math.max(1, maxRandom));
         randomTimer.setInitialDelay(nextDelay);
         randomTimer.restart();
         gamePanel.repaint();
-        System.out.println("HI");
     }
 
     public int getScore() {
         return this.score;
     }
 
-    public void changePlacement() {
-        Random r = new Random();
-        //sets x and y to random point
-
-        this.coords[0] = r.nextInt(Math.max(1, 580 - squareSize));
-        this.coords[1] = r.nextInt(Math.max(1, 580 - squareSize));
-
-       // this.coords[0] = r.nextInt((int) (-UISize * (score / 5)), (int) UISize * (score / 5));
-        //this.coords[1] = r.nextInt((int) (-UISize * (score / 5)), (int) UISize * (score / 5));
-    }
-
-    public int[] getCoords() {
-        return coords;
-    }
-
-    public int getSize() {
-        return this.squareSize;
-    }
-
     public JPanel getGamePanel() {
         return gamePanel;
+    }
+
+    //thanks gemini..
+
+    // --- Inner class to hold data for individual squares ---
+    private class SquareEntity {
+        boolean isRed = true;
+        int squareSize;
+        int[] coords = new int[2];
     }
 }
