b057c8ba18e55d3c1ae75e4d468e428748a79abe
Daniel <dm390@st-andrews.ac.uk>
2026-03-28 14:46:23 +0000

    Tested timer and got it displaying time correctly

--- General/Timer.java
+++ General/Timer.java
@@ -1,8 +1,14 @@
+package General;
+
 import java.time.Duration;
 import java.time.Instant;
 
 public class Timer {
     private Instant start;
+
+    public Timer() {
+        setStartToNow();
+    }
 
     public void setStartToNow() {
         start = Instant.now();
@@ -10,6 +16,17 @@ public class Timer {
 
     public String timeSinceStart() {
         Duration duration =  Duration.between(start, Instant.now());
-        return String.format("%d:%d:%d", duration.toMinutes(), duration.toSecondsPart(), duration.toMillisPart());
+        String minutesStr = padNumberAsString(duration.toMinutes(), 1);
+        String secondsStr = padNumberAsString(duration.toSecondsPart(), 2);
+        String millisStr = padNumberAsString(duration.toMillisPart(), 3);
+        return String.format("%s:%s:%s", minutesStr, secondsStr, millisStr);
+    }
+
+    public static String padNumberAsString(long num, int minLen) {
+        String numStr = Long.toString(num);
+        while (numStr.length() < minLen) {
+            numStr = "0" + numStr;
+        }
+        return numStr;
     }
 }
