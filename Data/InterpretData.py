709c7c53d64651d5bcb311d550832684f4b768d3
blop <blopblop913@gmail.com>
2026-03-28 23:11:44 +0000

    Added matplotlib.

--- Data/InterpretData.py
+++ Data/InterpretData.py
@@ -1,5 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
 def run():
-    print("Package logic executing...")
+    print("Generating plot...")
+    xpoints = np.array([0, 6])
+    ypoints = np.array([0, 250])
+
+    plt.plot(xpoints, ypoints)
+
+    # Save to a file in your project folder instead of dumping to console
+    plt.savefig("output_plot.png")
+    print("Plot saved to output_plot.png")
 
 if __name__ == "__main__":
     run()
